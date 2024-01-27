535a93fdf300cbc1d8c9bce6e4903331b5360e90
Mark J Koch <maehem@users.noreply.github.com>
2024-01-27 13:14:58 -0800

    WIP.  Plunge and Draw need work.

--- Firmware/epoch-fw/mode_8.ino
+++ Firmware/epoch-fw/mode_8.ino
@@ -172,7 +172,7 @@ void mode_8_loop() {
     if (!wastouched) {  // New touch
       pDragStart.x = p.x;
       pDragStart.y = p.y;
-      Serial.printf("Mode 8 touch: %d\n", ui.getItemPressed(p));
+      //Serial.printf("Mode 8 touch: %d\n", ui.getItemPressed(p));
       switch (mode_8_function) {                      // Handle menu
         case 0:                                       // Mode 0 top menu
           mode_8_0_handle_btn(ui.getItemPressed(p));  // will change the mode or function.
@@ -534,6 +534,7 @@ void mode_8_2_update_motors() {
   float wav5 = sin(mode_8_2_cycle + VIB_PI * 1.25);  // 225deg phase.
   float wav6 = sin(mode_8_2_cycle + VIB_PI * 1.50);  // 270deg phase.
   float wav7 = sin(mode_8_2_cycle + VIB_PI * 1.75);  // 315deg phase.
+  //Serial.printf( "wav0:%0.3f\n", wav0 );
   // note: wav->mot sequence is 0,2,4,6,7,6,5,3,1
   float mot0 = (wav0 + 1.0f) / 2.0f * m_8_2_slider[0].getVal(); // tip 1
   float mot1 = (wav0 + 1.0f) / 2.0f * m_8_2_slider[0].getVal(); // tip 1
@@ -578,6 +579,7 @@ void mode_8_3_update_motors() {  // Pendulum
   if (mode_8_3_cycle > VIB_PI * 2.0) {
     mode_8_3_cycle = 0.0;
   }
+
   // calc new motor values
   // vibeVal = (sin(advanceRads) + 1) * vibMax/2
   float wav0 = sin(mode_8_3_cycle);
@@ -585,6 +587,7 @@ void mode_8_3_update_motors() {  // Pendulum
   float wav2 = sin(mode_8_3_cycle + VIB_PI * 0.8);
   float wav3 = sin(mode_8_3_cycle + VIB_PI * 1.2);
   float wav4 = sin(mode_8_3_cycle + VIB_PI * 1.6);
+  Serial.printf( "wav0:%0.3f\n", wav0 );
   // note: wav->mot sequence is 0,1,3,2
   float mot0 = (wav0 + 1.0f) / 2.0f * m_8_3_slider[0].getVal(); // tip 1
   float mot1 = (wav1 + 1.0f) / 2.0f * m_8_3_slider[0].getVal(); // tip 2
@@ -610,36 +613,82 @@ void mode_8_4_update_motors() {  // Plunge
   float adv;
   if (m_8_4_slider[3].getVal() > 0) {
     adv = ((MODE_8_4_CYC_MAX - MODE_8_4_CYC_MIN) / (m_8_4_slider[3].getMax() - 1.0) * (m_8_4_slider[3].getVal() - 1.0));
-    adv /= 20.0;
+    adv /= 10.0;
   } else {
     adv = 0.0;
   }
 
   mode_8_4_cycle += adv;
-  if (mode_8_4_cycle > VIB_PI*1.2) { // Arc over just a bit.
+
+  if (mode_8_4_cycle > VIB_PI*2.0) {
     mode_8_4_cycle = 0.0;
   }
+  //Serial.printf( "cyc:%0.3f\n", mode_8_4_cycle );
+
   // calc new motor values
   // vibeVal = (sin(advanceRads) + 1) * vibMax/2
-  float wav0 = sin(mode_8_4_cycle);
-  float wav1 = sin(mode_8_4_cycle + VIB_PI * 0.25);
-  float wav2 = sin(mode_8_4_cycle + VIB_PI * 0.5);
-  float wav3 = sin(mode_8_4_cycle + VIB_PI * 0.75);
-  float wav4 = sin(mode_8_4_cycle + VIB_PI);
+  // float wav0 = cos(mode_8_4_cycle);
+  // float wav1 = cos(mode_8_4_cycle + VIB_PI * 0.25);
+  // float wav2 = cos(mode_8_4_cycle + VIB_PI * 0.5);
+  // float wav3 = cos(mode_8_4_cycle + VIB_PI * 0.75);
+  // float wav4 = cos(mode_8_4_cycle + VIB_PI);
+    
+    float wavA = cos(mode_8_4_cycle);
+    float wavB = sin(mode_8_4_cycle);
+    float wav0 = wavA;
+    float wav1 = wavB;
+    float wav2 = 0.0;
+    float wav3 = 0.0;
+    float wav4 = 0.0;
+    if ( wav0 < 0.0 ) {
+      wav2 = -wav0;
+      wav0 = 0.0;
+    } else {
+      wav2 = 0.0;
+      if ( mode_8_4_cycle > VIB_PI*1.5 ) {
+        wav4 = wav0;
+        wav0 = 0.0;
+      }
+    }
+    if ( wav1 < 0 ) {
+      wav3 = -wav1;
+      wav1 = 0.0;
+    } else {
+      wav3 = 0.0;
+    }
+
+  // Serial.printf( "A:%0.3f\n", abs(wavA) );
+  // Serial.printf( "B:%0.3f\n", abs(wavB) );
+  // Serial.printf( "wav0:%0.3f\n", wav0 );
+  // Serial.printf( "wav1:%0.3f\n", wav1 );
+  // Serial.printf( "wav2:%0.3f\n", wav2 );
+  // Serial.printf( "wav3:%0.3f\n", wav3 );
+  // Serial.printf( "wav4:%0.3f\n", wav4 );
+
   // note: wav->mot sequence is 0,1,3,2
   float mot0 = (wav0 + 1.0f) / 2.0f * m_8_4_slider[0].getVal(); // tip 1
   float mot1 = (wav1 + 1.0f) / 2.0f * m_8_4_slider[0].getVal(); // tip 2
   float mot2 = (wav2 + 1.0f) / 2.0f * m_8_4_slider[1].getVal(); // base
   float mot3 = (wav3 + 1.0f) / 2.0f * m_8_4_slider[1].getVal(); // mid
   float mot4 = (wav4 + 1.0f) / 2.0f * m_8_4_slider[2].getVal(); // rear
-  vibes.set(0, mot0);
-  vibes.set(1, mot0);
-  vibes.set(2, mot1);
-  vibes.set(3, mot1);
+  // vibes.set(0, mot0);
+  // vibes.set(1, mot0);
+  // vibes.set(2, mot1);
+  // vibes.set(3, mot1);
+  // vibes.set(4, mot2);
+  // vibes.set(5, mot2);
+  // vibes.set(6, mot3);
+  // vibes.set(7, mot4);
+
+  vibes.set(0, mot4);
+  vibes.set(1, mot4);
+  vibes.set(2, mot3);
+  vibes.set(3, mot3);
   vibes.set(4, mot2);
   vibes.set(5, mot2);
-  vibes.set(6, mot3);
-  vibes.set(7, mot4);
+  vibes.set(6, mot1);
+  vibes.set(7, mot0);
+
   vibes.pack();
 }
 
@@ -651,34 +700,74 @@ void mode_8_5_update_motors() {  // Draw
   float adv;
   if (m_8_5_slider[1].getVal() > 0) {
     adv = ((MODE_8_4_CYC_MAX - MODE_8_4_CYC_MIN) / (m_8_5_slider[3].getMax() - 1.0) * (m_8_5_slider[3].getVal() - 1.0));
-    adv /= 20.0;
+    adv /= 10.0;
   } else {
     adv = 0.0;
   }
 
   mode_8_5_cycle += adv;
-  if (mode_8_5_cycle > VIB_PI*1.2) { // Arc over just a bit.
+  if (mode_8_5_cycle > VIB_PI*2.0) { // Arc over just a bit.
     mode_8_5_cycle = 0.0;
   }
   // calc new motor values
-  float wav0 = sin(mode_8_5_cycle);
-  float wav1 = sin(mode_8_5_cycle + VIB_PI * 0.25);
-  float wav2 = sin(mode_8_5_cycle + VIB_PI * 0.5);
-  float wav3 = sin(mode_8_5_cycle + VIB_PI * 0.75);
-  float wav4 = sin(mode_8_5_cycle + VIB_PI);
+  // float wav0 = cos(mode_8_5_cycle);
+  // float wav1 = cos(mode_8_5_cycle + VIB_PI * 0.25);
+  // float wav2 = cos(mode_8_5_cycle + VIB_PI * 0.5);
+  // float wav3 = cos(mode_8_5_cycle + VIB_PI * 0.75);
+  // float wav4 = cos(mode_8_5_cycle + VIB_PI);
+
+    float wavA = cos(mode_8_5_cycle);
+    float wavB = sin(mode_8_5_cycle);
+    float wav0 = wavA;
+    float wav1 = wavB;
+    float wav2 = 0.0;
+    float wav3 = 0.0;
+    float wav4 = 0.0;
+    if ( wav0 < 0.0 ) {
+      wav2 = -wav0;
+      wav0 = 0.0;
+    } else {
+      wav2 = 0.0;
+      if ( mode_8_4_cycle > VIB_PI*1.5 ) {
+        wav4 = wav0;
+        wav0 = 0.0;
+      }
+    }
+    if ( wav1 < 0 ) {
+      wav3 = -wav1;
+      wav1 = 0.0;
+    } else {
+      wav3 = 0.0;
+    }
+  // Serial.printf( "wav0:%0.3f\n", wav0 );
+  // Serial.printf( "wav1:%0.3f\n", wav1 );
+  // Serial.printf( "wav2:%0.3f\n", wav2 );
+  // Serial.printf( "wav3:%0.3f\n", wav3 );
+  // Serial.printf( "wav4:%0.3f\n", wav4 );
 
   float mot0 = (wav0 + 1.0f) / 2.0f * m_8_5_slider[0].getVal(); // rear
   float mot1 = (wav1 + 1.0f) / 2.0f * m_8_5_slider[0].getVal(); // mid
   float mot2 = (wav2 + 1.0f) / 2.0f * m_8_5_slider[1].getVal(); // base
   float mot3 = (wav3 + 1.0f) / 2.0f * m_8_5_slider[1].getVal(); // tip 2
   float mot4 = (wav4 + 1.0f) / 2.0f * m_8_5_slider[2].getVal(); // tip 1
-  vibes.set(0, mot4);
-  vibes.set(1, mot4);
-  vibes.set(2, mot3);
-  vibes.set(3, mot3);
+
+  // vibes.set(0, mot4);
+  // vibes.set(1, mot4);
+  // vibes.set(2, mot3);
+  // vibes.set(3, mot3);
+  // vibes.set(4, mot2);
+  // vibes.set(5, mot2);
+  // vibes.set(6, mot1);
+  // vibes.set(7, mot0);
+
+  vibes.set(0, mot0);
+  vibes.set(1, mot0);
+  vibes.set(2, mot1);
+  vibes.set(3, mot1);
   vibes.set(4, mot2);
   vibes.set(5, mot2);
-  vibes.set(6, mot1);
-  vibes.set(7, mot0);
+  vibes.set(6, mot3);
+  vibes.set(7, mot4);
+
   vibes.pack();
 }
